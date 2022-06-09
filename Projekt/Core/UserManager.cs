c39b8fd24042a63fe1caebcda2e10037a41cabce
masciuch <102214@g.elearn.uz.zgora.pl>
2022-06-09 21:28:19 +0200

    Clearify Code

--- Projekt/Core/UserManager.cs
+++ Projekt/Core/UserManager.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Projekt.Core
+﻿namespace Projekt.Core
 {
     public class UserManager
     {
--- Projekt/GUI/OpenFileForm.cs
+++ Projekt/GUI/OpenFileForm.cs
@@ -1,11 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
 using System.Windows.Forms;
 
 namespace Projekt.GUI
@@ -72,7 +66,7 @@ namespace Projekt.GUI
 
         private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
         {
-            System.Diagnostics.Process.Start("https://github.com/masciuch/DotNetProject");
+            Process.Start("https://github.com/masciuch/DotNetProject");
         }
     }
 } 
